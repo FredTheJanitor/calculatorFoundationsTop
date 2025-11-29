2b5369f406be6c16d501b53f40e5bac07b9f9cc3
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-11-29 18:30:00 -0500

    feat: add some more variables

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 // instead of cramming input into weird small functions...
 // lets lean into javascript's internal calculation system which works fine.
+let input;
+let solution;
+let previousCalculation;
 const inputInStorage = [];
 const inputInDisplay = [];
-let solution;
+const calculator = {
+    storage: inputInStorage,
+    display: inputInDisplay
+};
+
 
 function storeInput() {
-    
-
+    inputInStorage.push(input);
 }
 function displayInput() {
-
+    inputInDisplay.push(input);
+    // display input
 }
 function evaluate() {
+    // run calculation within store input
+    // trigger display to reflect previous calculation and solution at same time
 
 }
