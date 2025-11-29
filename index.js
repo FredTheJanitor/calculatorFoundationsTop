95342618f7dc6328ad3ab1d2e787d58c05d58fc3
fredthejanitor <199515797+FredTheJanitor@users.noreply.github.com>
2025-11-29 18:30:00 -0500

    feat: begin bones of functions for project

---
+++ index.js
@@ -0,0 +1,16 @@
+// instead of cramming input into weird small functions...
+// lets lean into javascript's internal calculation system which works fine.
+const inputInStorage = [];
+const inputInDisplay = [];
+let solution;
+
+function storeInput() {
+    
+
+}
+function displayInput() {
+
+}
+function evaluate() {
+
+}
